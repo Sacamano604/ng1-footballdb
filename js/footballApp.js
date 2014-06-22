27e9feb670c56ae065560fa5aeca30a616a187fb
Sacamano604 <bentoussi@gmail.com>
2014-06-22 14:52:10 -0700

    AngularJS - Day 4 Complete
    Previous day was marked as 3, but should have been 4

---
+++ js/footballApp.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var footballApp = angular.module('footballApp', []);
+    footballApp.controller('footballController', function ($scope, $http){
+      $http.get('footballTeams.json').success(function(data){
+        $scope.teams = data;
+      });
+    $scope.sortField = 'name';
+    $scope.reverse = true;
+      });
