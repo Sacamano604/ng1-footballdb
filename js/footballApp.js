b222a2abfa84ec3dcee7e18791343a3eb0220f00
Sacamano604 <bentoussi@gmail.com>
2014-06-25 07:06:29 -0700

    AngularJS - Day 5 Complete
    Lots of edits. Now has back end for parsing JSON from the mySQL database

--- js/footballApp.js
+++ js/footballApp.js
@@ -2,7 +2,7 @@
 
 var footballApp = angular.module('footballApp', []);
     footballApp.controller('footballController', [ "$scope", "$http", function ($scope, $http){
-      $http.get({method: 'GET', url: 'SQLtoJSON.php'}).success(function(data){
+      $http({method: 'GET', url: 'teams.php'}).success(function(data){
         $scope.teams = data;
       });
     $scope.sortField = 'name';
