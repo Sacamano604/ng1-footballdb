d3beb19b707da396392f91ceba2578caa631a355
Sacamano604 <bentoussi@gmail.com>
2014-07-08 11:10:24 -0700

    Preventing SQL Injection
    Changed PHP code to prevent injections

--- teams/teams.php
+++ teams/teams.php
@@ -1,67 +1,37 @@
 <?php
-//Set variabloes and locations for the connection.
-$hostname = "localhost";
-$user = "bentouss_testing";
-$pass = "b3ntoussi";
-$database = "bentouss_testing";
-//connection string and die if fails
-mysql_connect("$hostname", "$user", "$pass") or die(mysql_error());
-mysql_select_db("$database") or die(mysql_error());
+$mysqli = mysqli_connect("localhost", "bentouss_testing", "b3ntoussi", "bentouss_testing");
 
 
 switch($_GET["action"]){
 	case "list":
-		$sql = "SELECT * FROM teamList";
-		$result = mysql_query($sql);
-		if (mysql_num_rows ($result)) {
-		while ($row = mysql_fetch_assoc($result)) {
+		$query = "SELECT * FROM teamList";
+		$result = $mysqli->query($query);
+		while ($row = $result->fetch_assoc()) {
 			$json[] = array('id' => $row['id'], 'name' => $row['name'], 'founded' => $row['founded'], 'city' => $row['city'], 'stadium' => $row['stadium'], 'capacity' => $row['capacity'], 'manager' => $row['manager'], 'websiteLink' => $row['websiteLink'], 'image' => $row['image'], 'details' => $row['details']);
-			}
-		}	
+		}
 		echo json_encode($json);
+		$mysqli->close();
 	break;
 
 	case "detail":
-		if (isset($_GET['id'])) {
-			$id = $_GET['id'];
-			}
-			$sql = "SELECT * FROM teamList WHERE id = '$id'";
-			$result = mysql_query($sql);
-			if (mysql_num_rows ($result)) {
-				if ($row = mysql_fetch_assoc($result)) {
-					$json = array('id' => $row['id'], 'name' => $row['name'], 'founded' => $row['founded'], 'city' => $row['city'], 'stadium' => $row['stadium'], 'capacity' => $row['capacity'], 'manager' => $row['manager'], 'websiteLink' => $row['websiteLink'], 'image' => $row['image'], 'details' => $row['details']);
-					}
-			}	
-			echo json_encode($json);
+	 	$id = $_GET['id'];
+	 	$stmt = $mysqli->stmt_init();
+		$query = $mysqli->prepare('SELECT * FROM teamList WHERE id = ?');
+		$query->bind_param('i', $id);
+		$query->execute();
+		$result = $query->get_result();
+		while ($row = $result->fetch_assoc()){
+		 	$json = array('id' => $row['id'], 'name' => $row['name'], 'founded' => $row['founded'], 'city' => $row['city'], 'stadium' => $row['stadium'], 'capacity' => $row['capacity'], 'manager' => $row['manager'], 'websiteLink' => $row['websiteLink'], 'image' => $row['image'], 'details' => $row['details']);
+		 }
+		 echo json_encode($json);
+		 $query->close();
 	break;
 
 	case "add":
-
-	//	$uploadFolder = "/badges/";
-	//	$uploadFile = basename($_FILES['image']['image']); 
-	//	$uploadPath = $uploadFolder . $uploadFile;
-
-	//	move_uploaded_file($_FILES['image']['tmp_name'], $uploadPath);
-
-
-		$name = $_POST['name'];
-		$founded = $_POST['founded'];
-		$city = $_POST['city'];
-		$stadium = $_POST['stadium'];
-		$capacity = $_POST['capacity'];
-		$manager = $_POST['manager'];
-		$websiteLink = $_POST['websiteLink'];
-		$image = $_POST['image'];
-		$details = $_POST['details'];
-		//$json = file_get_contents("php://input");
-		//$result = json_decode($json, true);
-		
-
-		
-
-		
-		$sql = "INSERT INTO teamList (name, founded, city, stadium, capacity, manager, websiteLink, image, details) VALUES ('$name', '$founded', '$city', '$stadium', '$capacity', '$manager', '$websiteLink', '$image', '$details')";
-		mysql_query($sql) or die(mysql_error());
+		$query = $mysqli->prepare('INSERT INTO teamList (name, founded, city, stadium, capacity, manager, websiteLink, image, details) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)');
+		$query->bind_param('sssssssss', $_POST['name'], $_POST['founded'], $_POST['city'], $_POST['stadium'], $_POST['capacity'], $_POST['manager'], $_POST['websiteLink'], $_POST['image'], $_POST['details']);
+		$query->execute();
+		$query->close();
 	break;
 
 }
